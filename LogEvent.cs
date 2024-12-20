63487c3c3a9c36ff8c44692be696cf3d95d7c6fb
Arno Dewald <arno.dewald@wassermann-group.com>
2024-12-20 09:18:35 +0100

    Initial version

---
+++ LogEvent.cs
@@ -0,0 +1,65 @@
+﻿using TCEVENTLOGGERLib;
+
+namespace TcEventLog.Wrapper
+{
+    public class LogEvent
+    {
+        /// <summary>
+        /// 8 = Paramterfehler, 7 = Error, 6 = Warning, 5 = Instruktion, 4 = Information, 3 = Hinweis, 2 = Nachricht, 1 = Wartung, 0 = Unbekannt
+        /// </summary>
+        public enum EventClasses
+        {
+            Unbekannt = 0,
+            Wartung = 1,
+            Nachricht = 2,
+            Hinweis = 3,
+            Information = 4,
+            Instruktion = 5,
+            Warning = 6,
+            Error = 7,
+            Parameterfehler = 8
+        }
+
+        public int SrcId { get; }
+        public int EventId { get; }
+        public EventClasses EventClass { get; }
+        public int MsConfirmed { get; }
+        public int MsReset { get; }
+
+        ITcEvent? _TtcEvent = null;
+
+        internal LogEvent(ITcEvent tcEvent)
+        {
+            _TtcEvent = tcEvent;
+            SrcId = tcEvent.SrcId;
+            EventId = tcEvent.Id;
+            EventClass = (EventClasses)tcEvent.Class;
+            MsConfirmed = tcEvent.MsConfirmed;
+            MsReset = tcEvent.MsReset;
+        }
+
+        public string? GetSourceName(int langId)
+        {
+            try
+            {
+                return _TtcEvent?.SourceName[langId];
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public string? GetMsgstring(int langId)
+        {
+            try
+            {
+                return _TtcEvent?.GetMsgString(langId);
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ TcEventLogWrapper.cs
@@ -0,0 +1,56 @@
+﻿namespace TcEventLog.Wrapper
+{
+    public class TcEventLogWrapper : IDisposable
+    {
+        TCEVENTLOGGERLib.TcEventLog? _TcLogger = null;
+
+        public event Action<LogEvent>? OnNewLogEvent;
+
+        public void ConnectLocalPC()
+        {
+            _TcLogger = new TCEVENTLOGGERLib.TcEventLog();
+            _TcLogger.OnNewEvent += M_logger_OnNewEvent;
+            _TcLogger.OnConfirmEvent += M_logger_OnConfirmEvent;
+            _TcLogger.OnResetEvent += M_logger_OnResetEvent;
+
+            foreach (TCEVENTLOGGERLib.TcEvent evt in _TcLogger.EnumActiveEventsEx())
+            {
+                RaiseLogEvent(evt);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (_TcLogger != null)
+            {
+                _TcLogger.OnNewEvent -= M_logger_OnNewEvent;
+                _TcLogger.OnConfirmEvent -= M_logger_OnConfirmEvent;
+                _TcLogger.OnResetEvent -= M_logger_OnResetEvent;
+                _TcLogger = null;
+            }
+        }
+
+        void RaiseLogEvent(object evt)
+        {
+            var tce = evt as TCEVENTLOGGERLib.ITcEvent;
+            if (tce != null)
+            {
+                OnNewLogEvent?.Invoke(new LogEvent(tce));
+            }
+        }
+
+        private void M_logger_OnResetEvent(object evtObj)
+        {
+            RaiseLogEvent(evtObj);
+        }
+
+        private void M_logger_OnConfirmEvent(object evtObj)
+        {
+            RaiseLogEvent(evtObj);
+        }
+        private void M_logger_OnNewEvent(object evtObj)
+        {
+            RaiseLogEvent(evtObj);
+        }
+    }
+}
